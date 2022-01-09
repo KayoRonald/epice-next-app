932f3b2a3be80adbb9339e3656092063acd86855
Kyler Kai (Kayo Ronald) <kayoronaldmashiro@gmail.com>
2022-01-09 11:25:26 +0000

    feat: serverless-mysql

---
+++ src/connection/index.tsx
@@ -0,0 +1,9 @@
+import mysql from 'serverless-mysql';
+export const db = mysql({
+  config: {
+    host: process.env.MYSQL_HOST,
+    database: process.env.MYSQL_DATABASE,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD
+  }
+});
