7fc251084b9ad2fa902a53382a2abee7e45ba2be
Kayo Ronald <kayoronald4321@gmail.com>
2022-01-11 08:44:25 -0300

    config

--- src/connection/index.tsx
+++ src/connection/index.tsx
@@ -1,10 +1,8 @@
 // conexão com banco de daods: https://www.remotemysql.com/phpmyadmin/
 import mysql from 'serverless-mysql';
-export const db = mysql({
-  config: {
+export const db = mysql.config({
     host: process.env.MYSQL_HOST,
     database: process.env.MYSQL_DATABASE,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD
-  }
 });
