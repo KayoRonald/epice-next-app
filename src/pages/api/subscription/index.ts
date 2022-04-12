1cc6a64d4a75373a8cc7f0ac33694591ada9091b
Kayler <kayoronald.github17@gmail.com>
2022-04-12 00:04:16 +0000

    fix: Security Update sql

--- src/pages/api/subscription/index.ts
+++ src/pages/api/subscription/index.ts
@@ -1,25 +1,35 @@
 import type { NextApiResponse } from 'next'
-import { db } from '../../../connection'
-import { ExtendedNextApiRequest } from '../../../utils/interface'
+import * as yup from 'yup'
+import excuteQuery from '../../../lib/db';
+import { ExtendedNextApiRequest } from '../../../utils/interface';
+
+const schema = yup.object().shape({
+  name: yup.string().required(),
+  email: yup.string().email("Email inválido.").required("Esse campo é obrigatório."),
+  curso: yup.string()
+})
 
 export default async (req: ExtendedNextApiRequest, res: NextApiResponse) => {
   const { name, email, curso } = req.body
-  try {
-    const results = await db.query(
-      `INSERT INTO EPICEDB (name,email,curso) VALUES('${name}', '${email}', '${curso}')`
-    );
-    await db.end();
-    console.log(results)
-    return res.status(200).send('')
-  } catch (error: any) {
-    console.log(error.code);
-    if (error.code === 'ER_DUP_ENTRY') {
-      return res.status(406).json({
-        message: 'Este email já está cadastrado em nosso banco de dados'
+  if ((await schema.isValid(req.body))) {
+    try {
+      const result: any = await excuteQuery('INSERT INTO EPICEDB (name, email, curso) VALUES (?)', [[name, email, curso]]);
+      res.status(200).json({
+        success: true,
+        data: result,
+        message: `Inscrição realizada com sucesso!`
+      });
+      console.log(result)
+    } catch (error: any) {
+      return res.status(400).json({
+        error: true,
+        status: 'error',
+        message: error.message
       });
     }
+  } else {
     return res.status(400).json({
-      message: 'Falha na conexão code erro `EMAIL-300`'
+      message: "Campos inválidos! Verifique se está tudo preenchido"
     });
   }
-};
+}
