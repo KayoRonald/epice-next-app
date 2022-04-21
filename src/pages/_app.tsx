cf12dd30b632290373d13fadf248b3b068df45a7
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2022-04-21 18:34:33 -0300

    feat: PWA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { useEffect } from 'react'
 import { AppProps } from 'next/app'
 import { ApolloProvider } from '@apollo/client'
 import { Chakra } from '../theme/'
@@ -9,6 +10,13 @@ import { Footer } from '@/container/index'
 import { useApollo } from '@/utils/apollo'
 
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js')
+      window.addEventListener('beforeinstallprompt', () => {})
+    }
+  })
+
   const client = useApollo(pageProps.initialApolloState)
 
   return (
@@ -17,6 +25,8 @@ const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
         <Head>
           <meta name="viewport" content="viewport-fit=cover" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="theme-color" content="#8257e5" />
+          <link rel="manifest" href="/manifest.json" />
         </Head>
         <NavBar />
         <Component {...pageProps} />
