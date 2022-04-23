eb5a989df41a99a57da82f963c1b0768d423355d
Kayo Ronald <kayoronald4321@gmail.com>
2022-04-23 19:59:10 -0300

    refactor: adding icons input page contact

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 import {
   Flex,
   Box, FormControl, FormLabel,
-  Textarea, Button, useColorModeValue, useToast, Input as ChakraInput, FormErrorMessage
+  Textarea, Button, useColorModeValue,
+  useToast, Input as ChakraInput, 
+  FormErrorMessage,
 } from '@chakra-ui/react';
 import Head from 'next/head'
 import {
@@ -15,10 +17,11 @@ import axios from 'axios'
 import { useForm, SubmitHandler } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup";
-import { InputProps } from "../utils/types";
-import { FormHeader } from '../components/';
+import { InputProps } from "@/utils/types";
+import { FormHeader } from '@/components/index';
+import { Input } from "@/components/Input";
 const Contact: React.FC = () => {
-  // const toast = useToast()
+  const toast = useToast()
 
   type FormValues = {
     name: string;
@@ -43,51 +46,31 @@ const Contact: React.FC = () => {
       .required("Esse campo é obrigatório"),
   })
   const [loading, setLoading] = React.useState<boolean>(false);
-  // async function handleSubmit(event: React.FormEvent): Promise<void> {
-  //   event.preventDefault();
-  //   const data = {
-  //     name,
-  //     email,
-  //     subject,
-  //     message
-  //   };
-  //   if (name !== '' && email !== '' && subject !== '') {
-  //     try {
-  //       setLoading(true)
-  //       await axios.post('/api/contact', data)
-  //       toast({
-  //         title: 'Enviado com sucesso!.',
-  //         description: "Aguarde alguns dias para que possamos lhe ajudar.",
-  //         status: 'success',
-  //         duration: 9000,
-  //         isClosable: true,
-  //       })
-  //     } catch (error: any) {
-  //       toast({
-  //         title: 'Não foi possível cadastrar.',
-  //         description: error.message || error,
-  //         status: 'error',
-  //         duration: 5000,
-  //         isClosable: true,
-  //       })
-  //     } finally {
-  //       setLoading(false)
-  //     }
-  //   } else {
-  //     toast({
-  //       title: 'Não foi possível cadastrar.',
-  //       description: "Certifique-se de que todos os campos estejam preenchidos.",
-  //       status: 'error',
-  //       duration: 5000,
-  //       isClosable: true,
-  //     })
-  //   }
-  // }
   const { register, handleSubmit, formState: { errors } } = useForm<FormValues>({
     resolver: yupResolver(schema)
   });
   async function onSubmitHandler<SubmitHandler, FormValues>(data: FormValues): Promise<void> {
-    console.log(data)
+    try {
+      setLoading(true)
+      await axios.post('/api/contact', data)
+      toast({
+        title: 'Enviado com sucesso!.',
+        description: "Aguarde alguns dias para que possamos lhe ajudar.",
+        status: 'success',
+        duration: 9000,
+        isClosable: true,
+      })
+    } catch (error: any) {
+      toast({
+        title: 'Não foi possível cadastrar.',
+        description: error.message || error,
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+      })
+    } finally {
+      setLoading(false)
+    }
   };
   return (
     <React.Fragment key="key">
@@ -123,6 +106,8 @@ const Contact: React.FC = () => {
                 <Input
                   type="text"
                   placeholder="Insira o seu nome"
+                  iconleft={<AiOutlineUserDelete />}
+                  iserror={errors.email ? true : undefined}
                   {...register('name')}
                 />
                 <FormErrorMessage>
@@ -134,6 +119,8 @@ const Contact: React.FC = () => {
                 <Input
                   type="email"
                   placeholder="Insira o seu endereço de e-mail"
+                  iconleft={<AiOutlineMail />}
+                  iserror={errors.email ? true : undefined}
                   {...register('email')}
                 />
                 <FormErrorMessage>
@@ -145,6 +132,8 @@ const Contact: React.FC = () => {
                 <Input
                   type="text"
                   placeholder="Insira a sua descrição"
+                  iconleft={<AiFillFileText />}
+                  iserror={errors.subject ? true : undefined}
                   {...register('subject')}
                 />
                 <FormErrorMessage>
@@ -187,18 +176,5 @@ const ButtonSend = () => {
     </Button>
   );
 };
-const Input = React.forwardRef<HTMLInputElement, InputProps>((props, ref) => (
-  <ChakraInput
-    ref={ref}
-    type={props.type}
-    focusBorderColor="purple.500"
-    backgroundColor={useColorModeValue("white", "gray.800")}
-    border={0}
-    color={useColorModeValue("#18216d", "white")}
-    _placeholder={{
-      color: useColorModeValue("black", "white"),
-    }}
-    {...props}
-  />
-));
+
 export default Contact;
