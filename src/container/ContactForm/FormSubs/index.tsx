fcd162093520bef6405815e6138e76ef2e66eeb8
Kayo Ronald <kayoronald4321@gmail.com>
2022-04-23 19:06:22 -0300

    initial commit

--- src/container/ContactForm/FormSubs/index.tsx
+++ src/container/ContactForm/FormSubs/index.tsx
@@ -1,55 +1,66 @@
 /* eslint-disable */
-import React from 'react'
+import React from "react";
 import {
-  Box, Stack, Heading, Button,
-  useColorModeValue, useToast, Select, FormLabel, FormControl,
-  FormErrorMessage,
-} from '@chakra-ui/react';
-import { AiOutlineSend, AiOutlineUserAdd, AiOutlineMail } from 'react-icons/ai'
-import { useForm } from 'react-hook-form'
-import { yupResolver } from '@hookform/resolvers/yup';
-import { ContainerInput } from '@/components/index';
-import axios from 'axios';
-import { Input } from '@/components/Input';
-import { schema } from 'src/schema/index';
-import { FormValues } from '@/utils/types';
+  Box,
+  Stack,
+  Heading,
+  Button,
+  useColorModeValue,
+  useToast,
+  Select,
+  FormLabel,
+  FormControl,
+  FormErrorMessage
+} from "@chakra-ui/react";
+import { AiOutlineSend, AiOutlineUserAdd, AiOutlineMail } from "react-icons/ai";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { ContainerInput } from "@/components/index";
+import axios from "axios";
+import { Input } from "@/components/Input";
+import { schema } from "src/schema/index";
+import { FormValues } from "@/utils/types";
 
 export default function FormSubscription() {
   const [loading, setLoading] = React.useState<boolean>(false);
-  const toast = useToast()
-  const { register, handleSubmit, formState: { errors } } = useForm<FormValues>({
+  const toast = useToast();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormValues>({
     resolver: yupResolver(schema)
   });
   async function onSubmitHandler<FormValues>(data: FormValues): Promise<void> {
     try {
-      setLoading(true)
-      await axios.post('/api/subscription', data)
+      setLoading(true);
+      await axios.post("/api/subscription", data);
       toast({
-        title: 'Cadastrado com sucesso.',
+        title: "Cadastrado com sucesso.",
         description: "Aguarde alguns dias para realização do evento.",
-        status: 'success',
+        status: "success",
         duration: 9000,
-        isClosable: true,
-      })
-    } catch (error: any) {
-      let message: string = error.response.data.message   
-      console.log(message)
+        isClosable: true
+      });
+    } catch (error) {
+      let message: string = error.response.data.message;
+      console.log(message);
       toast({
-        title: 'Não foi possível cadastrar.',
+        title: "Não foi possível cadastrar.",
         description: message,
-        status: 'error',
+        status: "error",
         duration: 5000,
-        isClosable: true,
-      })
+        isClosable: true
+      });
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-  };
+  }
 
   return (
     <Stack
-      bg={useColorModeValue('#d0d8dd', '#000')}
-      rounded={'xl'}
+      bg={useColorModeValue("#d0d8dd", "#000")}
+      rounded={"xl"}
       p={{ base: 4, sm: 6, md: 8 }}
       spacing={{ base: 8 }}
       width="94%"
@@ -59,71 +70,59 @@ export default function FormSubscription() {
       <Box as="form" mt={10} onSubmit={handleSubmit(onSubmitHandler)}>
         <Stack spacing={4}>
           <FormControl isInvalid={!!errors.name}>
-            <ContainerInput
-              txt="Coloque seu Nome Completo"
-            >
+            <ContainerInput txt="Coloque seu Nome Completo">
               <Input
                 placeholder="Ex: João Nobrega"
                 type="text"
-                {...register('name')}
                 iconleft={<AiOutlineUserAdd />}
                 iserror={errors.name ? true : undefined}
+                {...register("name")}
               />
             </ContainerInput>
-            <FormErrorMessage>
-              {errors.name?.message}
-            </FormErrorMessage>
+            <FormErrorMessage>{errors.name?.message}</FormErrorMessage>
           </FormControl>
           <FormControl isInvalid={!!errors.email}>
-            <ContainerInput
-              txt="Coloque seu Email"
-            >
+            <ContainerInput txt="Coloque seu Email">
               <Input
                 placeholder="Ex: exemplo@gmail.com"
                 type="text"
-                {...register('email')}
                 iserror={errors.email ? true : undefined}
                 iconleft={<AiOutlineMail />}
+                {...register("email")}
               />
             </ContainerInput>
-            <FormErrorMessage>
-              {errors.email?.message}
-            </FormErrorMessage>
+            <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
           </FormControl>
           <FormControl isInvalid={!!errors.curso}>
-            <FormLabel
-              color={useColorModeValue("#18216d", "white")}
-            >
+            <FormLabel color={useColorModeValue("#18216d", "white")}>
               Escolha o Curso que você faz
             </FormLabel>
             <Select
               placeholder="Ex: INFO, Eletrônica, Visitante..."
-              {...register("curso")}
               backgroundColor={useColorModeValue("white", "gray.800")}
+              {...register("curso")}
             >
               <option value="INFO">INFO</option>
               <option value="Eletrônica">Eletrônica</option>
               <option value="Visitante">Visitante</option>
             </Select>
-            <FormErrorMessage>
-              {errors.curso?.message}
-            </FormErrorMessage>
+            <FormErrorMessage>{errors.curso?.message}</FormErrorMessage>
           </FormControl>
           <Button
             type="submit"
             width="full"
             mt={4}
             outline="none"
-            _hover={{ backgroundColor: 'purple.600' }}
+            _hover={{ backgroundColor: "purple.600" }}
             backgroundColor="purple.500"
             leftIcon={<AiOutlineSend />}
             isLoading={loading}
-            loadingText='Submitting'
+            loadingText="Submitting"
           >
             Realizar inscrição
           </Button>
         </Stack>
       </Box>
     </Stack>
-  )
+  );
 }
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,94 +1,79 @@
 /* eslint-disable */
-import React from 'react';
+import React from "react";
 import {
   Flex,
-  Box, FormControl, FormLabel,
-  Textarea, Button, useColorModeValue, useToast, Input as ChakraInput, FormErrorMessage
-} from '@chakra-ui/react';
-import Head from 'next/head'
+  Box,
+  FormControl,
+  FormLabel,
+  Textarea,
+  Button,
+  useColorModeValue,
+  useToast,
+  Input as ChakraInput,
+  FormErrorMessage
+} from "@chakra-ui/react";
+import Head from "next/head";
 import {
   AiOutlineUserDelete,
   AiOutlineMail,
-  AiFillFileText,
-} from 'react-icons/ai';
-import axios from 'axios'
-import { useForm, SubmitHandler } from 'react-hook-form'
-import { yupResolver } from '@hookform/resolvers/yup';
+  AiFillFileText
+} from "react-icons/ai";
+import axios from "axios";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
-import { InputProps } from "../utils/types";
-import { FormHeader } from '../components/';
+import { FormHeader } from "@/components/index";
+import { Input } from "@/components/Input";
 const Contact: React.FC = () => {
-  // const toast = useToast()
-
+  const toast = useToast();
   type FormValues = {
     name: string;
     email: string;
     subject: string;
     message: string;
   };
-
   const schema = yup.object().shape({
-    name: yup
-      .string()
-      .required("Esse campo é obrigatório"),
+    name: yup.string().required("Esse campo é obrigatório"),
     email: yup
       .string()
       .email("Email inválido")
       .required("Esse campo é obrigatório"),
-    subject: yup
-      .string()
-      .required("Esse campo é obrigatório"),
-    message: yup
-      .string()
-      .required("Esse campo é obrigatório"),
-  })
+    subject: yup.string().required("Esse campo é obrigatório"),
+    message: yup.string().required("Esse campo é obrigatório")
+  });
   const [loading, setLoading] = React.useState<boolean>(false);
-  // async function handleSubmit(event: React.FormEvent): Promise<void> {
-  //   event.preventDefault();
-  //   const data = {
-  //     name,
-  //     email,
-  //     subject,
-  //     message
-  //   };
-  //   if (name !== '' && email !== '' && subject !== '') {
-  //     try {
-  //       setLoading(true)
-  //       await axios.post('/api/contact', data)
-  //       toast({
-  //         title: 'Enviado com sucesso!.',
-  //         description: "Aguarde alguns dias para que possamos lhe ajudar.",
-  //         status: 'success',
-  //         duration: 9000,
-  //         isClosable: true,
-  //       })
-  //     } catch (error: any) {
-  //       toast({
-  //         title: 'Não foi possível cadastrar.',
-  //         description: error.message || error,
-  //         status: 'error',
-  //         duration: 5000,
-  //         isClosable: true,
-  //       })
-  //     } finally {
-  //       setLoading(false)
-  //     }
-  //   } else {
-  //     toast({
-  //       title: 'Não foi possível cadastrar.',
-  //       description: "Certifique-se de que todos os campos estejam preenchidos.",
-  //       status: 'error',
-  //       duration: 5000,
-  //       isClosable: true,
-  //     })
-  //   }
-  // }
-  const { register, handleSubmit, formState: { errors } } = useForm<FormValues>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormValues>({
     resolver: yupResolver(schema)
   });
-  async function onSubmitHandler<SubmitHandler, FormValues>(data: FormValues): Promise<void> {
-    console.log(data)
-  };
+  async function onSubmitHandler<SubmitHandler, FormValues>(
+    data: FormValues
+  ): Promise<void> {
+    try {
+      setLoading(true);
+      await axios.post("/api/contact", data);
+      toast({
+        title: "Enviado com sucesso!.",
+        description: "Aguarde alguns dias para que possamos lhe ajudar.",
+        status: "success",
+        duration: 9000,
+        isClosable: true
+      });
+    } catch (error) {
+      toast({
+        title: "Não foi possível cadastrar.",
+        description: error.message || error,
+        status: "error",
+        duration: 5000,
+        isClosable: true
+      });
+    } finally {
+      setLoading(false);
+    }
+  }
   return (
     <React.Fragment key="key">
       <Head>
@@ -105,7 +90,7 @@ const Contact: React.FC = () => {
         <Box
           as="form"
           onSubmit={handleSubmit(onSubmitHandler)}
-          backgroundColor={useColorModeValue('#d0d8dd', '#0e0e10')}
+          backgroundColor={useColorModeValue("#d0d8dd", "#0e0e10")}
           px={4}
           width="94%"
           maxWidth="500px"
@@ -113,43 +98,40 @@ const Contact: React.FC = () => {
           textAlign="center"
         >
           <Box p={4}>
-            <FormHeader
-              heading="Contato"
-              text="Detalhe o seu problema"
-            />
+            <FormHeader heading="Contato" text="Detalhe o seu problema" />
             <Box my={8} textAlign="left">
               <FormControl id="nome" isInvalid={!!errors.name}>
                 <FormLabel>Nome:</FormLabel>
                 <Input
                   type="text"
                   placeholder="Insira o seu nome"
-                  {...register('name')}
+                  iconleft={<AiOutlineUserDelete />}
+                  iserror={errors.name ? true : undefined}
+                  {...register("name")}
                 />
-                <FormErrorMessage>
-                  {errors.name?.message}
-                </FormErrorMessage>
+                <FormErrorMessage>{errors.name?.message}</FormErrorMessage>
               </FormControl>
               <FormControl id="email" mt={2} isInvalid={!!errors.email}>
                 <FormLabel>E-mail:</FormLabel>
                 <Input
                   type="email"
                   placeholder="Insira o seu endereço de e-mail"
-                  {...register('email')}
+                  iconleft={<AiOutlineMail />}
+                  iserror={errors.email ? true : undefined}
+                  {...register("email")}
                 />
-                <FormErrorMessage>
-                  {errors.email?.message}
-                </FormErrorMessage>
+                <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
               </FormControl>
               <FormControl id="descricao" mt={2} isInvalid={!!errors.subject}>
                 <FormLabel>Descrição:</FormLabel>
                 <Input
                   type="text"
                   placeholder="Insira a sua descrição"
-                  {...register('subject')}
+                  iconleft={<AiFillFileText />}
+                  iserror={errors.subject ? true : undefined}
+                  {...register("subject")}
                 />
-                <FormErrorMessage>
-                  {errors.subject?.message}
-                </FormErrorMessage>
+                <FormErrorMessage>{errors.subject?.message}</FormErrorMessage>
               </FormControl>
               <FormControl id="text" mt={2} isInvalid={!!errors.message}>
                 <FormLabel>Texto:</FormLabel>
@@ -159,11 +141,9 @@ const Contact: React.FC = () => {
                   backgroundColor={useColorModeValue("white", "gray.800")}
                   border={0}
                   color={useColorModeValue("#18216d", "white")}
-                  {...register('message')}
+                  {...register("message")}
                 />
-                <FormErrorMessage>
-                  {errors.message?.message}
-                </FormErrorMessage>
+                <FormErrorMessage>{errors.message?.message}</FormErrorMessage>
               </FormControl>
               <ButtonSend />
             </Box>
@@ -180,25 +160,12 @@ const ButtonSend = () => {
       width="full"
       mt={4}
       outline="none"
-      _hover={{ backgroundColor: 'purple.600' }}
+      _hover={{ backgroundColor: "purple.600" }}
       backgroundColor="purple.500"
     >
       Enviar
     </Button>
   );
 };
-const Input = React.forwardRef<HTMLInputElement, InputProps>((props, ref) => (
-  <ChakraInput
-    ref={ref}
-    type={props.type}
-    focusBorderColor="purple.500"
-    backgroundColor={useColorModeValue("white", "gray.800")}
-    border={0}
-    color={useColorModeValue("#18216d", "white")}
-    _placeholder={{
-      color: useColorModeValue("black", "white"),
-    }}
-    {...props}
-  />
-));
+
 export default Contact;
